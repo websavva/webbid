3e406332cde9314f27b00b6592b671a0f343535a
WebSavva <websavvap@gmail.com>
2023-12-31 23:05:51 +0300

    fix: NavBar was slighly fixed

--- src/components/ui/NavBar/index.tsx
+++ src/components/ui/NavBar/index.tsx
@@ -7,10 +7,10 @@ export interface NavBarProps extends HTMLAttributes<HTMLElement> {}
 
 export function NavBar({}: NavBarProps) {
   return (
-    <nav className='flex justify-center'>
-      <Container className='flex justify-between items-center py-10 sticky top-0 border-b border-gray-100'>
+    <nav className='flex justify-center sticky top-0 bg-white'>
+      <Container className='flex justify-between items-center py-7 border-b-2 border-gray-100'>
         <div className='flex items-center'>
-          <Logo className='w-14 h-14' />
+          <Logo className='w-12 h-12' />
         </div>
 
         <div />
