9e0fec67c9d9f8d67a8a9c8819ed5140159693ff
WebSavva <websavvap@gmail.com>
2024-01-05 23:31:28 +0300

    feat: ShoppingCart was improved

--- src/components/ui/NavBar/ShoppingCart/index.tsx
+++ src/components/ui/NavBar/ShoppingCart/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ShoppingBagIcon } from 'lucide-react';
+import { ShoppingBagIcon, ShoppingBasketIcon } from 'lucide-react';
 
 import type { DefineProps } from '@/types';
 import {
@@ -13,10 +13,14 @@ import {
   SheetClose,
 } from '@/components/ui/Sheet';
 import { Button } from '@/components/ui/Button';
+import { formatPrice } from '@/lib/formatters';
+import Link from 'next/link';
 
 export type ShoppingCartProps = DefineProps<{}>;
 
 export function ShoppingCart({}: ShoppingCartProps) {
+  const itemsCount = 0;
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -36,11 +40,38 @@ export function ShoppingCart({}: ShoppingCartProps) {
           <SheetTitle>Cart (0)</SheetTitle>
         </SheetHeader>
 
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button>Checkout</Button>
-          </SheetClose>
-        </SheetFooter>
+        {itemsCount > 0 && (
+          <>
+            <div className='flex flex-col text-lg space-y-5 my-5'>
+              <div className='flex items-center justify-between'>
+                <span>Shipping</span>
+                <span>{formatPrice(0)}</span>
+              </div>
+            </div>
+
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button className='w-full'>Checkout</Button>
+              </SheetClose>
+            </SheetFooter>
+          </>
+        )}
+
+        {itemsCount === 0 && (
+          <>
+            <div className='my-8 text-center flex flex-col items-center text-slate-500'>
+              <ShoppingBasketIcon className='w-14 h-14 mb-5' />
+
+              <span className='text-xl mb-3'>Cart is empty</span>
+            </div>
+
+            <SheetFooter>
+              <SheetClose asChild>
+                <Link href='/catalog' className='mx-auto text-blue-500'>Go to catalog</Link>
+              </SheetClose>
+            </SheetFooter>
+          </>
+        )}
       </SheetContent>
     </Sheet>
   );
