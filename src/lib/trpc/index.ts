79d0b9933e8982b8ac99acf6b259bd232f4a0ea8
WebSavva <websavvap@gmail.com>
2024-01-24 00:20:33 +0300

    feat: trpcClient was created

---
+++ src/lib/trpc/index.ts
@@ -0,0 +1,11 @@
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+
+import type { AppRouter } from '@/server/trpc/types';
+
+export const trpcClient = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: process.env.NEXT_PUBLIC_SERVER_URL! + '/trpc',
+    }),
+  ],
+});
