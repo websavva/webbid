19be57d5ba1a2f9a03116af65c0f90d0a6563f32
WebSavva <websavvap@gmail.com>
2024-01-28 23:59:22 +0300

    feat: headers are used

--- src/lib/trpc/index.ts
+++ src/lib/trpc/index.ts
@@ -1,5 +1,5 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import {} from 'next/headers';
+import { defu } from 'defu';
 
 import type { AppRouter } from '@/server/trpc/types';
 
@@ -7,6 +7,19 @@ export const trpcClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
       url: process.env.NEXT_PUBLIC_SERVER_URL! + '/api/trpc',
+
+      headers({ opList: operations }) {
+        let aggregatedHeaders: Record<string, string[] | string> = {};
+
+        for (const {
+          context: { headers = {} },
+        } of operations) {
+          aggregatedHeaders = defu(aggregatedHeaders, headers as any);
+        }
+
+        return aggregatedHeaders;
+      },
+
       fetch(url, options) {
         return fetch(url, {
           ...options,
@@ -16,3 +29,5 @@ export const trpcClient = createTRPCProxyClient<AppRouter>({
     }),
   ],
 });
+
+trpcClient.auth.login.mutate({}, {});
