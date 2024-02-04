21ed46ac75db4276d457e7aa91e48aee1d68e51b
WebSavva <websavvap@gmail.com>
2024-02-04 23:44:59 +0300

    Another style improvement was made

--- src/app/(auth)/sign-up/confirm/[token]/page.tsx
+++ src/app/(auth)/sign-up/confirm/[token]/page.tsx
@@ -1,28 +1,41 @@
 'use client';
-import { useRouter } from 'next/router';
 
 import { Loader2Icon, AlertCircleIcon } from 'lucide-react';
 
 import type { PagePropsWithParams } from '@/types/page-props';
+import { Button } from '@/components/ui/Button';
 
 export default function SignUpVerifyPage({
   params: { token },
 }: PagePropsWithParams<{
   token: string;
 }>) {
+  let isLoading = false;
+
   return (
     <div className='py-20'>
-      {/* <div className='flex flex-col items-center'>
-        <Loader2Icon className='size-44 animate-spin stroke-1 text-slate-600' />
+      {isLoading && (
+        <div className='flex flex-col items-center'>
+          <Loader2Icon className='size-44 animate-spin stroke-1 text-slate-600' />
 
-        <span className='mt-6 font-light text-lg'>
-          Confirming your account...
-        </span>
-      </div> */}
+          <span className='mt-6 font-light text-lg'>
+            Confirming your account...
+          </span>
+        </div>
+      )}
+      {!isLoading && (
+        <div className='flex flex-col items-center'>
+          <AlertCircleIcon className='size-44 text-red-400 stroke-1' />
 
-      <div className='flex flex-col items-center'>
-        <AlertCircleIcon className='size-44 text-gray-500 stroke-1'/>
-      </div>
+          <span className='mt-6 font-light text-lg'>
+            Ops, something went wrong..
+          </span>
+
+          <Button className='text-base mt-10' variant={'secondary'}>
+            Try again
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
