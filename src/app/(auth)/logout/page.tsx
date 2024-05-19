c74e98057dc8a73f03a6eae700346626fd0b05ff
WebSavva <websavvap@gmail.com>
2024-05-19 21:37:20 +0300

    feat: Default error page was implemented

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useEffect } from 'react';
 import { Loader2Icon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
---
+++ src/app/error.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Container } from '@/components/ui/Container';
+import { ErrorIcon } from '@/components/ui/icons/ErrorIcon';
+import { TRPCClientError } from '@trpc/client';
+
+interface AppErrorPageProps {
+  error: Error;
+}
+
+export default function AppErrorPage({ error }: AppErrorPageProps) {
+  let message: string;
+
+  if (error instanceof TRPCClientError) {
+    const { code } = error.data;
+
+    switch (code) {
+      case 'UNAUTHORIZED':
+        message = 'You have not been authenticated !';
+        break;
+      default:
+        message = error.message;
+    }
+  } else {
+    message = error.message;
+  }
+
+  return (
+    <Container className='flex flex-col items-center py-16 mx-auto'>
+      <ErrorIcon className='max-w-lg' />
+
+      <div className='mt-8 text-xl text-center font-semibold text-gray-800'>
+        {message}
+      </div>
+    </Container>
+  );
+}
