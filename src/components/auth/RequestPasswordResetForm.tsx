416f74581362c3f28075e749e5a467b0df37a7c3
WebSavva <websavvap@gmail.com>
2024-08-21 00:13:14 +0300

    feat: TransitionFade was added in PasswordReset page

--- src/components/auth/RequestPasswordResetForm.tsx
+++ src/components/auth/RequestPasswordResetForm.tsx
@@ -8,6 +8,7 @@ import { trpcClient } from '@/lib/trpc';
 import { UserCredentialsDto } from '#server/dtos/auth';
 import { AuthFormFrame } from '@/components/AuthFormFrame';
 
+import TransitionFade from '../UI/TransitionFade';
 import { SentEmailIcon } from '../Icons/SentEmailIcon';
 
 import { AuthForm } from './AuthForm';
@@ -15,7 +16,7 @@ import { AuthForm } from './AuthForm';
 export const RequestPasswordResetForm = () => {
   const [email, setEmail] = useState<string | null>(null);
 
-  const { isSuccess, makeApiCall } = useApi(
+  const { isSuccess, makeApiCall, status } = useApi(
     ({ email }: UserCredentialsDto) => {
       return trpcClient.auth.requestPasswordReset.mutate({ email });
     },
@@ -26,36 +27,43 @@ export const RequestPasswordResetForm = () => {
         }),
 
       onSuccess: ({ email }) => setEmail(email),
-    }
+    },
   );
 
-  if (isSuccess)
-    return (
-      <div className='flex flex-col items-center'>
-        <SentEmailIcon className='size-80 sm:size-96 lg:size-[500px]' />
-
-        <div className='mt-5 font-semibold sm:text-lg w-4/5 sm:w-3/5 leading-relaxed text-center text-gray-700'>
-          Letter with password reset instruction was sent{' '}
-          {email && (
-            <span>
-              to your email <span className='text-primary'>{email}</span>{' '}
-            </span>
-          )}
-        </div>
-      </div>
-    );
-
   return (
-    <AuthFormFrame
-      linkHref='/sign-up'
-      linkText="Don't have an account ? Sign Up"
-      heading='Password Reset'
-    >
-      <AuthForm
-        submitButtonText='Submit'
-        selectedFields={['email']}
-        onSubmit={makeApiCall}
-      />
-    </AuthFormFrame>
+    <TransitionFade transitionKey={status} className='w-full'>
+      {(status) => {
+        if (status === 'success') {
+          return (
+            <div className='flex flex-col items-center'>
+              <SentEmailIcon className='size-80 sm:size-96 lg:size-[500px]' />
+
+              <div className='mt-5 font-semibold sm:text-lg w-4/5 sm:w-3/5 leading-relaxed text-center text-gray-700'>
+                Letter with password reset instruction was sent{' '}
+                {email && (
+                  <span>
+                    to your email <span className='text-primary'>{email}</span>{' '}
+                  </span>
+                )}
+              </div>
+            </div>
+          );
+        } else {
+          return (
+            <AuthFormFrame
+              linkHref='/sign-up'
+              linkText="Don't have an account ? Sign Up"
+              heading='Password Reset'
+            >
+              <AuthForm
+                submitButtonText='Submit'
+                selectedFields={['email']}
+                onSubmit={makeApiCall}
+              />
+            </AuthFormFrame>
+          );
+        }
+      }}
+    </TransitionFade>
   );
 };
