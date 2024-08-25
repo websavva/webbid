f616656e90835f28b98d7055db652c731dab17fa
WebSavva <websavvap@gmail.com>
2024-08-25 23:23:36 +0300

    feat(metadata): meta tags were added for Cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,21 @@
-import { Cart } from "@/components/Cart";
+import type { Metadata } from 'next';
+
+import { Cart } from '@/components/Cart';
+
+export const metadata: Metadata = {
+  title: 'Your cart',
+  description: '',
+
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function CartPage() {
-  return <div className="mx-auto max-w-[90%] sm:max-w-2xl py-10 sm:py-16">
-    <Cart isPage />
-  </div>
+  return (
+    <div className='mx-auto max-w-[90%] sm:max-w-2xl py-10 sm:py-16'>
+      <Cart isPage />
+    </div>
+  );
 }
