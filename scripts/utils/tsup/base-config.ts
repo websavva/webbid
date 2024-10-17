6d4b2e4f02a5707a1af95cbc8091d461193972e6
WebSavva <websavvap@gmail.com>
2024-10-17 12:39:16 +0300

    fix(build): All NEXT_PUBLIC env vars were moved to build env vars

--- scripts/utils/tsup/base-config.ts
+++ scripts/utils/tsup/base-config.ts
@@ -2,14 +2,14 @@ import type { Options } from 'tsup';
 import { defu } from 'defu';
 
 import { JsxRuntimAutoImportPlugin } from './plugins/jsx-runtime-auto-import';
-import { builEnvDefine } from './env-define';
+import { buildEnvDefine } from './env-define';
 
 export const baseTsupConfig: Options = {
   outDir: 'dist',
   format: 'cjs',
 
   esbuildPlugins: [JsxRuntimAutoImportPlugin],
-  define: builEnvDefine,
+  define: buildEnvDefine,
 };
 
 export const extendBaseTsupConfig = (options: Options = {}) =>
