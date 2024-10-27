c24170e53d89e5d593194bc51509fd7d07a0acaf
WebSavva <websavvap@gmail.com>
2024-10-27 23:53:13 +0300

    test: Unit tests were written for loadEnv

--- src/server/env/loader.test.ts
+++ src/server/env/loader.test.ts
@@ -4,6 +4,10 @@ import { loadEnv } from './loader';
 
 const mockedDotenvConfig = vi.hoisted(() => vi.fn());
 
+vi.stubGlobal('process', {
+  cwd: () => '/root',
+});
+
 vi.mock('dotenv', () => ({
   default: {
     config: mockedDotenvConfig,
@@ -12,14 +16,74 @@ vi.mock('dotenv', () => ({
 
 describe('loadEnv', () => {
   beforeEach(() => {
-    mockedDotenvConfig.mockClear()
+    mockedDotenvConfig.mockClear();
   });
 
-  it('should aplly correct default value', () => {
+  it('should apply correct default value', () => {
     loadEnv();
 
+    expect(mockedDotenvConfig.mock.calls).toHaveLength(2);
     expect(mockedDotenvConfig.mock.calls).toEqual([
+      [
+        {
+          path: '/root/.env.build',
+        },
+      ],
+      [
+        {
+          path: '/root/.env',
+        },
+      ],
+    ]);
+  });
 
-    ])
+  it('should load build env', () => {
+    loadEnv(['build']);
+
+    expect(mockedDotenvConfig.mock.calls).toHaveLength(1);
+    expect(mockedDotenvConfig.mock.calls[0]).toEqual([
+      {
+        path: '/root/.env.build',
+      },
+    ]);
+  });
+
+  it('should load runtim env', () => {
+    loadEnv(['runtime']);
+
+    expect(mockedDotenvConfig.mock.calls).toHaveLength(1);
+    expect(mockedDotenvConfig.mock.calls[0]).toEqual([
+      {
+        path: '/root/.env',
+      },
+    ]);
+  });
+
+  it('should load duplicated envs', () => {
+    loadEnv(['runtime', 'build', 'runtime', 'build']);
+
+    expect(mockedDotenvConfig.mock.calls).toHaveLength(4);
+    expect(mockedDotenvConfig.mock.calls).toEqual([
+      [
+        {
+          path: '/root/.env',
+        },
+      ],
+      [
+        {
+          path: '/root/.env.build',
+        },
+      ],
+      [
+        {
+          path: '/root/.env',
+        },
+      ],
+      [
+        {
+          path: '/root/.env.build',
+        },
+      ],
+    ]);
   });
 });
